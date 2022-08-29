c9164cb1cd4f4ebb508853b23782cd1e5519a2d6
hkve <haakon.kvernmoen@gmail.com>
2022-08-29 14:56:06 +0200

    Determinants should be calculated using LU decomp

--- sandkasse/determinant.py
+++ sandkasse/determinant.py
@@ -1,5 +1,6 @@
 import numpy as np
 import itertools
+import scipy.linalg
 
 n = 3
 A = np.random.uniform(low=-1, high=1, size=(n,n))
@@ -8,7 +9,6 @@ def determinant(A):
 	n, m = A.shape
 	cols = np.arange(m)
 	det = 0
-
 
 	# Loop over permutations
 	# For all permutation, get sign(?), and multiply over i A[i][permuted index]
@@ -19,12 +19,25 @@ def determinant(A):
 		for i, j in enumerate(perm):
 			row *= A[i,j]
 			diffs += (i != j)
-		
-		det += (-1)**(diffs) * row
+
+		sgn = (-1)**np.abs(diffs-1)
+		if diffs == 0: sgn *= -1
+
+		det += sgn * row
 
 	return det 
 
+def determinant_lu(A):
+	n, m = A.shape
+	P, L, U = scipy.linalg.lu(A)
+	det = 1
+	for i in range(n):
+		det *= L[i,i]*U[i,i]
+
+	return det*np.linalg.det(P)
+
 a = np.linalg.det(A)
 b = determinant(A)
+c = determinant_lu(A)
 
-print(a, b)
+print(a, b, c)
