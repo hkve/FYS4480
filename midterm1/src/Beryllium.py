b23c6b9540ce4f0902b2feb4e820b5dfaf7f1a82
hkve <haakon.kvernmoen@gmail.com>
2022-10-23 12:56:40 +0200

    Texed in some values and formatting code

--- midterm1/src/Beryllium.py
+++ midterm1/src/Beryllium.py
@@ -7,22 +7,23 @@ a_order = [(2,True),(2,False),(2,True),(2,False)]
 i_order = [(0,True),(0,False),(1,True),(1,False)]
 Fermi = Indices(1)
 
-# Beryllium = Atom(M, Fermi, i_order, a_order).fill()
-# print(Beryllium)
-# Beryllium.solve()
-# print(Beryllium.E)
+print("Configuration interaction")
+Beryllium = Atom(M, Fermi, i_order, a_order).fill()
+print(Beryllium)
+Beryllium.solve()
+print(f"CI: E = {Beryllium.E.min()}\n")
 
+print("Hartree-Fock")
 HaFo = HartreeFock(M, Np=4)
-HaFo.solve(tol=1e-8, maxiters=1000)
+HaFo.solve(tol=1e-12, maxiters=10000)
 print(HaFo)
-print(HaFo.E_gs)
+print(f"HF: E = {HaFo.E_gs}\n")
 
+Z = 4
+v = M.elms
+E = - 5*Z**2 / 4
+E += (
+    v[0,0,0,0] + v[1,1,1,1] + 2*v[0,1,0,1] - v[0,1,1,0] + 2 *v[1,0,1,0] - v[1,0,0,1]
+)
 
-# Z = 4
-# v = M.elms
-# E = - 5*Z**2 / 4
-# E += (
-#     v[0,0,0,0] + v[1,1,1,1] + 2*v[0,1,0,1] - v[0,1,1,0] + 2 *v[1,0,1,0] - v[1,0,0,1]
-# )
-
-# print(E)
+print(f"E[Phi0] = {E:.4f}")
--- midterm1/src/Helium.py
+++ midterm1/src/Helium.py
@@ -3,29 +3,18 @@ import numpy as np
 
 M = MatrixElements(N=3, Z=2).read()
 
-# a_order = [(1,True),(1,False),(2,True),(2,False)]
-# i_order = [(0,True),(0,False),(0,True),(0,False)]
+a_order = [(1,True),(1,False),(2,True),(2,False)]
+i_order = [(0,True),(0,False),(0,True),(0,False)]
 Fermi = Indices(0)
 
-# Helium = Atom(M, Fermi, i_order, a_order).fill()
-# print(Helium)
-# Helium.solve()
-# print(Helium.E)
+print("Configuration interaction")
+Helium = Atom(M, Fermi, i_order, a_order).fill()
+print(Helium)
+Helium.solve()
+print(f"CI: E = {Helium.E.min()}\n")
 
-
+print("Hartree-Fock")
 HaFo = HartreeFock(M)
-HaFo.solve(tol=1e-16, maxiters=10000)
+HaFo.solve(tol=1e-12, maxiters=10000)
 print(HaFo)
-print(HaFo.E_gs)
-
-# hf0 = np.zeros(shape=(6,6))
-# All = [_ for _ in Fermi.below()] + [_ for _ in Fermi.above()]
-
-# # Expected after first iteration
-# for i, I in enumerate(All):
-#     for j, J in enumerate(All):
-#         hf0[i,j] = M.get_onebody(I)*(i == j)
-#         for beta in Fermi.below():
-#             hf0[i,j] += M.getAS(I, beta, J, beta)
-
-# print(hf0)
+print(f"HF: E = {HaFo.E_gs}")
